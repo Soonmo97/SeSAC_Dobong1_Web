94a62a39a5390b6795086ef5a4da4b7259bd06e9
soonmo <smkwon97@gmail.com>
2024-02-14 16:41:08 +0900

    24/02/14 wed - MVC_MySQL

---
+++ 12_mvc_mysql/controller/Cvisitor.js
@@ -0,0 +1,56 @@
+const Visiotor = require('../model/Visitor');
+
+exports.main = (req, res) => {
+  res.render('index');
+};
+
+exports.getVisitors = (req, res) => {
+  // visitor.ejs에 데이터 보내주기
+  // [DB 연결 전, 임시 데이터베이스]
+  // console.log(Visiotor.getVisitors()); //[{}, {}]
+  // res.render('visitor', {
+  //   data: Visiotor.getVisitors(),
+  // });
+  // [DB 연결 후]
+  Visiotor.getVisitors((result) => {
+    console.log('Cvisitor.js 전체목록::', result);
+    res.render('visitor', { data: result });
+  });
+};
+
+// GET /visitor/:id
+exports.getVisitor = (req, res) => {
+  console.log(req.params);
+  console.log(req.params.id);
+
+  Visiotor.getVisitor(req.params.id, (result) => {
+    console.log('Cvisitor.js 데이터 하나 조회', result); // {}
+    res.send(result);
+  });
+};
+
+// POST /visitor
+exports.postVisitor = (req, res) => {
+  console.log(req.body); // {id, name, comment}
+  Visiotor.postVisitor(req.body, (result) => {
+    console.log('Cvisitor.js post', result);
+    res.send({ id: result, name: req.body.name, comment: req.body.comment });
+  });
+};
+
+// DELETE /visitor
+exports.deleteVisitor = (req, res) => {
+  console.log(req.body);
+  console.log(req.body.id);
+  Visiotor.deleteVisitor(req.body.id, (result) => {
+    console.log('Cvisitor.js delete', result);
+    res.send(`${req.body.id} 번 방명록 삭제 완료!`);
+  });
+};
+
+// PATCH /visitor
+exports.patchVisitor = (req, res) => {
+  console.log('Cvisitor.js patch', req.body); // {id, name, comment}
+  Visiotor.patchVisitor(req.body, (result) => {});
+  res.send('수정 완료');
+}; // exports.getVisitors = ();
---
+++ 12_mvc_mysql/init.sql
@@ -0,0 +1,34 @@
+-- Active: 1707101284088@@127.0.0.1@3306@sesac
+SHOW TABLES;
+
+CREATE Table visitor (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(10) NOT NULL,
+  comment MEDIUMTEXT
+);
+
+DROP Table visitor;
+
+DESC visitor
+
+INSERT INTO visitor(name, comment) VALUES ('allie', 'hi~');
+INSERT INTO visitor VALUES (null,'홍길동', '홍길동입니다.');
+
+SELECT * FROM visitor
+
+#######[DCL]#######
+-- 새로운 user 만들기 1
+CREATE USER 'sesac'@'%' IDENTIFIED BY '1234';
+
+ALTER USER 'sesac'@'%' IDENTIFIED WITH mysql_native_password BY '1234';
+-- 비밀번호 바꾸고 싶다면
+
+-- 새로운 user만들기 2
+CREATE USER 'sesac'@'%' IDENTIFIED WITH mysql_native_password BY '1234';
+
+GRANT ALL PRIVILEGES ON *.* TO 'sesac'@'%' WITH GRANT OPTION;
+-- 모든 DB에 접근 가능하도록, sesac 계정에 DB접근 권한을 부여
+
+FLUSH PRIVILEGES;
+-- 현재 사용중인 mysql 캐시를 지우고 새로운 설정 적용
+
