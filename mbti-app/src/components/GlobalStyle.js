8e0d203e3d14ce1ec438eec326cc7cb0ce2f3fcb
soonmo <smkwon97@gmail.com>
2024-04-01 14:06:51 +0900

    24/04/01 Mon react-mbti-app

---
+++ mbti-app/src/components/GlobalStyle.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+
+@font-face {
+    font-family: 'ONE-Mobile-POP';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2105_2@1.0/ONE-Mobile-POP.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+  body {
+    font-family: 'ONE-Mobile-POP', sans-serif;
+  }
+  li{
+    list-style: none;
+  }
+`;
+
+export default GlobalStyle;
